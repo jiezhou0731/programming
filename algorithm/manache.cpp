917661b4b100aba0c99d1299972d94f8d1a73140
jie <jiezhou0731@gmail.com>
2019-11-20 22:01:48 -0800

    manache

--- algorithm/manache.cpp
+++ algorithm/manache.cpp
@@ -5,18 +5,8 @@
 #include <math.h> 
 #include <vector>
 #include <algorithm>
-#define ll long long
 using namespace std;
-using namespace __gnu_pbds;
-using namespace __gnu_cxx;
- 
-// less_equal if supports multiple items
-// order_of_key() find_by_order()
-typedef tree<long long, null_type, less_equal<long long>, rb_tree_tag, 
-             tree_order_statistics_node_update> 
-    dataSet; 
-const long long MOD = 100000000007;
-const long long M = 2*pow(10, 5)+100;
+
 string s;
 void calcL(string &s) {
     if (s.size() == 0) return;
@@ -30,6 +20,8 @@ void calcL(string &s) {
         if (diff > 0) {
             lps[currentRight] = min(diff, lps[currentLeft]);
         } 
+
+        // Expand based on current lps
         int expandLeft = currentRight - lps[currentRight] - 1;
         int expandRight = currentRight + lps[currentRight] + 1;
         while (expandLeft >= 0 && expandRight <= 2*s.size()
@@ -39,6 +31,8 @@ void calcL(string &s) {
             expandLeft--;
             expandRight++;
         }
+
+        // Update center right
         if (lps[currentRight] + currentRight > centerRight) {
             center = currentRight;
             centerRight = lps[currentRight] + currentRight;
