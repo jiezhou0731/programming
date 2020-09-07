7d5146d6114a6fef567dfd6e3bfcc7397f8d9c10
jiezhou0731 <jiezhou0731@gmail.com>
2020-09-07 12:49:34 -0700

    https://codeforces.com/problemset/problem/180/E
    https://codeforces.com/problemset/problem/180/E

--- algorithm/a.cpp
+++ algorithm/a.cpp
@@ -20,36 +20,7 @@ const long long INF = pow(2, 25);
 const long long M = pow(10,6);
 const ll MOD = 998244353;
 
-long long cost = 0;
-long long n, m, k, d, f, t, c, dmax;
-vector<tuple<ll,ll,ll,ll>> a;
 int main() {
-    scanf("%lld %lld %lld", &n, &m, &k);
-    dmax = 0;
-    for (int i = 0; i < m ;i++) {
-        scanf("%lld %lld %lld %lld",&d, &f, &t, &c);
-        dmax = max(d, dmax);
-        a.push_back(d,f,t,c);
-    }
-    sort(a.begin(), a.end());
-    ll ans = -1;
-    ll enterCnt = 0;
-    ll enterCost = 0;
-    multiset<ll> enter[M];
-    ll backCnt = 0;
-    ll backCost = 0;
-    multiset<ll> back[M];
-    for (ll i = 0; i < a.size(); i++) {
-        d = get<0>(a[i]);
-        f = get<1>(a[i]);
-        t = get<2>(a[i]);
-        c = get<3>(a[i]);
 
-    }
-
-    for (ll i = 1; i <= dmax - k + 1; i++) {
-
-    }
-    return 0;
 }
 
